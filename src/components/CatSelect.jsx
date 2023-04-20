5db332da79f5088f28d4e728781e7c1465a1dc55
Océane <oceanebrl.94@gmail.com>
2023-04-20 14:08:37 +0200

    select transformed in a toggle function

--- src/components/CatSelect.jsx
+++ src/components/CatSelect.jsx
@@ -1,24 +1,61 @@
 // cat = categories
 
 import style from "../styles/components/catSelect.module.scss";
+import chevron from "../assets/chevron.png";
+
+import { useState } from "react";
 
 function CatSelect({ activeCat, setActiveCat, cat }) {
+  const [toggle, setToggle] = useState(false);
+  const [selectedOption, setSelectedOption] = useState(null);
+
+  const handleToggle = () => {
+    setToggle(!toggle);
+  };
+
+  const handleSelected = (value) => () => {
+    setSelectedOption(value);
+    setActiveCat(value);
+    setToggle(false);
+  };
+
+  const resetOption = () => {
+    setSelectedOption(null);
+    setActiveCat("");
+    setToggle(false);
+  };
+
   return (
     <div className={style.catWrap}>
-      <select
-        className={style.cat}
-        value={activeCat}
-        onChange={(e) => setActiveCat(e.target.value)}>
-        <option value="">🌿🌱🌵 - Choisis le type de plante !</option>
-        {cat.map((c) => (
-          <option className={style.option} key={c} value={c}>
-            {c}
-          </option>
-        ))}
-      </select>
-      <button className={style.btn} onClick={() => setActiveCat("")}>
+      <div className={style.toggle}>
+        <div className={style.toggle__header} onClick={handleToggle}>
+          {selectedOption || "🌿🌱🌵 - Choisis le type de plante !"}
+          <img
+            className={
+              toggle
+                ? style.toggle__chevron
+                : `${style.toggle__chevron__closed} ${style.toggle__chevron}`
+            }
+            src={chevron}
+            alt="Chevron descendant pour la togglebar"
+          />
+        </div>
+        <div
+          className={
+            toggle
+              ? style.toggle__itemWrap
+              : `${style.toggle__itemWrap__closed} ${style.toggle__itemWrap}`
+          }>
+          {cat.map((c) => (
+            <div className={style.toggle__item} onClick={handleSelected(c)}>
+              {c}
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className={style.resetBtn} onClick={resetOption}>
         Réinitialiser
-      </button>
+      </div>
     </div>
   );
 }
